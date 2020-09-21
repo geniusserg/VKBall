9c1472df81e421b1a8aa0349c2b4dcff959b47da
geniusserg <danilovsergeydmitrievich@gmail.com.com>
2020-09-21 22:56:01 +0300

    initial version with auth

--- run_agent.py
+++ run_agent.py
@@ -1,4 +1,16 @@
-import time
+import vk_api
+import random
+import time 
 
 print("VK Volleyball initial")
+
+vk = vk_api.VkApi(token="", scope=4096)
+if not (vk):
+	print ("Failed auth")
+	wait = input()
+	exit()
+api = vk.get_api()
+print("Authentication successful")
+api.messages.send(user_id="", message= "[ " + str(time.time()) + " ] API v0.1 notification: service works", random_id=random.randint(1,1000000))
+print("Message sent successfully")
 wait = input()
