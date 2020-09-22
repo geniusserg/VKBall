1b829d3759536af63b0b17f5efd10f97a26a5f99
geniusserg <danilovsergeydmitrievich@gmail.com.com>
2020-09-22 16:21:07 +0300

    fetch latest

--- run_agent.py
+++ run_agent.py
@@ -2,15 +2,27 @@ import vk_api
 import random
 import time 
 
-print("VK Volleyball initial")
+####Variables############################
+access_token = ""
+group_user_id = ""
+group_hse_id = ""
+message = "Данилов Сергей 18ПИ-2"
+latest_record = {}
 
-vk = vk_api.VkApi(token="", scope=4096)
+####Code#################################
+
+print("VK Volleyball initial")
+vk = vk_api.VkApi(token=access_token, scope=4096)
 if not (vk):
 	print ("Failed auth")
 	wait = input()
 	exit()
 api = vk.get_api()
 print("Authentication successful")
-api.messages.send(user_id="", message= "[ " + str(time.time()) + " ] API v0.1 notification: service works", random_id=random.randint(1,1000000))
-print("Message sent successfully")
+
+for i in range(0, 3):
+	print("Fetch latest record from public")
+	latest_record = api.wall.get(owner_id=group_hse_id, count=1)
+	print("Message: "+latest_record[0]["id"]+" >>>  "+latest_record[0]["text"])
+	time.sleep(60)
 wait = input()
